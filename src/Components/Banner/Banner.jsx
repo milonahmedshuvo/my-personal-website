3d1960d4487902757ea3ce17b341c894931ed76b
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-06-20 04:35:36 +0600

    my website start

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Banner = () => {
+
+
+  return (
+    <div>
+      <p>Banner</p>
+     
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/Components/Navber/Navber.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navber = () => {
+
+ const manuItem = <>
+          <li> <Link className='text-[#858792] hover:text-[#ffffff] text-lg'>HOME</Link> </li>
+          <li> <Link className='text-[#858792] hover:text-[#ffffff] text-lg'>HOME</Link> </li>
+          <li> <Link className='text-[#858792] hover:text-[#ffffff] text-lg'>HOME</Link> </li>
+          <li> <Link className='text-[#858792] hover:text-[#ffffff] text-lg'>HOME</Link> </li>
+          <li> <Link className='text-[#858792] hover:text-[#ffffff] text-lg'>HOME</Link> </li>
+          <li> <Link className='text-[#858792] hover:text-[#ffffff] text-lg'>HOME</Link> </li>
+         
+                  
+ </>
+
+
+
+  return (
+    <div className="navbar  py-8  ">
+    <div className="navbar-start">
+      <div className="dropdown">
+        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        </label>
+
+
+        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+         
+             {manuItem}
+
+        </ul>
+
+
+
+      </div>
+      <a className="btn btn-ghost normal-case text-xl">My Website</a>
+    </div>
+    <div className="navbar-center hidden lg:flex">
+      <ul className="menu menu-horizontal px-1">
+        
+            {manuItem}
+      </ul>
+    </div>
+
+
+
+
+
+
+
+    <div className="navbar-end ">
+
+      <p className=" rounded-full border border-[#858792] px-12 py-3 hover:bg-[#2E303D] bg-[#252734] text-white text-lg ">Hire me</p>
+    </div>
+  </div>
+  )
+}
+
+export default Navber
